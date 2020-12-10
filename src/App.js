ba946b33fa5440878eb6b17565110ed3e3a34018
Brett Goulder <brett.goulder@gmail.com>
2020-12-09 23:24:21 -0800

    wip

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import Lorem from "./Lorem";
 
 function App() {
   return (
-    <div class="flex min-h-screen">
-      <nav className="flex flex-col items-center justify-between  bg-red-100 w-10">
-        {/* needs to not scroll */}
-        <div className="w-4 h-4 bg-green-500"></div>
-        <div className="w-4 h-4 bg-green-500"></div>
-      </nav>
-      <div class="flex flex-col flex-1  bg-blue-100">
-        <div className="h-10 bg-blue-500">{/* needs to be fixed */}</div>
-        <div className="flex">
-          <div className="w-32 bg-green-200 overflow-hidden">
-            {/* needs to be scrollable */}
-            <Lorem count={50} />
+    <div class="flex flex-row items-stretch w-screen h-screen">
+      <nav className="w-10 bg-red-100 flex-none">.</nav>
+      <div className="flex flex-col items-stretch w-full flex-auto overflow-hidden sm:overflow-auto">
+        <div className="bg-purple-300 flex-none h-12 sticky top-0">
+          search -- topmost nav --{" "}
+        </div>
+        <div className="flex sm:flex-row w-full flex-auto items-stretch overflow-y-auto sm:overflow-hidden flex-col">
+          <div className="bg-green-500 w-full sm:w-56 sm:flex-none sm:overflow-y-auto">
+            <p>top</p>
+            <Lorem count={70} />
           </div>
-          <div className="flex flex-col flex-1">
-            <div className="h-10 bg-yellow-50">{/* needs to be fixed */}</div>
-            <div className="flex-1">
-              {/* needs to be scrollable */}
-              <Lorem count={1000} />
-            </div>
+          <div className="bg-blue-500 w-full flex-auto sm:overflow-y-auto">
+            <p>foo</p>
+            <Lorem count={170} />
           </div>
         </div>
       </div>
